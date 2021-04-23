a4ff6d3479dc4320a3497650d6262a63cbab6ba6
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-04-23 15:33:09 +0200

    Add examples of calling primitives and doing string conversion from C++ to C#.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication
 {
@@ -10,9 +11,25 @@ namespace ConsoleApplication
         [DllImport("CppLibrary.dll")]
         public static extern void HelloWorld();
 
+        [DllImport("CppLibrary.dll")]
+        public static extern int Add(int x, int y);
+
+        /// <summary>
+        /// Mapping of C++ and C# primitives https://docs.microsoft.com/en-us/dotnet/framework/interop/marshaling-data-with-platform-invoke
+        /// </summary>
+        [DllImport("CppLibrary.dll")]
+        public static extern bool IsLengthGreaterThanN(string Value, int n);
+
+        [DllImport("CppLibrary.dll")]
+        [return: MarshalAs(UnmanagedType.BStr)]
+        public static extern string GetName();
+
         static void Main(string[] args)
         {
             HelloWorld();
+            Console.WriteLine(Add(5, 3));
+            Console.WriteLine($"Is 'Hello' more than 3 characters: {IsLengthGreaterThanN("Hello", 3)}");
+            Console.WriteLine($"My name is {GetName()}");
         }
     }
 }
--- CppLibrary/Source.cpp
+++ CppLibrary/Source.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <comdef.h>
 
 /// <summary>
 /// Have to specify as extern in order for outside (C#) programs to use.
 /// </summary>
 extern "C" __declspec(dllexport) void HelloWorld();
+extern "C" __declspec(dllexport) int Add(int a, int b);
+extern "C" __declspec(dllexport) bool IsLengthGreaterThanN(const char* string, int n);
+extern "C" __declspec(dllexport) BSTR GetName();
 
-void HelloWorld()
-{
-	std:: cout << "Hello World.";
+void HelloWorld() {
+	std:: cout << "Hello World.\n";
+}
+
+int Add(int a, int b) {
+	return a + b;
+}
+
+bool IsLengthGreaterThanN(const char* string, int n) {
+	return strlen(string) > n;
+}
+
+BSTR GetName() {
+	return SysAllocString(L"Willem");
 }
